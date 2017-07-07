123d200651391ca05703223093d0d0c56121f3d7
jmahhh <hubertjoel@gmail.com>
2017-07-07 11:42:36 +0800

    nav changes

--- imports/ui/components/documents/index/documents-index.js
+++ imports/ui/components/documents/index/documents-index.js
@@ -5,6 +5,9 @@ import { Documents } from '/imports/api/documents/both/document-collection.js'
 import './documents-index.html'
 import './documents-index-item/documents-index-item.js'
 
+const host = Meteor.settings.public.ethereumURL;
+const web3 = new Web3(new Web3.providers.HttpProvider(host));
+
 Template.documentsIndex.onCreated(function () {
   this.subscribe('documents.all')
 })
@@ -18,6 +21,10 @@ Template.documentsIndex.onDestroyed(function () {
 Template.documentsIndex.helpers({
   documents () {
     return Documents.find({}, { sort: { createdAt: -1 } })
+  },
+  showWeb3 () {
+    console.log('blocky!', web3.eth.getBlock('latest'));
+    return web3.eth.getBlock('latest');
   }
 })
 
